7513fb38799e2d26790ec6dd1390740bf6784012
SebberJJ <sebberjacobsen@gmail.com>
2024-11-20 10:46:09 +0100

    a few design bugs has been fixed

--- src/components/InfoCards.tsx
+++ src/components/InfoCards.tsx
@@ -4,7 +4,7 @@ export default function InfoCards() {
     return (
         <div className="flex justify-center items-center border-t-2 pt-16">
             <div className="flex w-1/5 mx-4">
-                <Image className="pb-16 mr-2" src="/property1.svg" alt="Salgstjek ikon" width={32} height={32} />
+                <Image className="mb-16 mr-2 bg-blue-100" src="/property1.svg" alt="Salgstjek ikon" width={32} height={32} />
                 <div className="flex flex-col">
                     <h3 className="font-bold text-blue-950">Bestil et salgstjek</h3>
                     <p className="w-fit">Med et Din Mægler Salgstjek 
@@ -13,7 +13,7 @@ export default function InfoCards() {
                 </div>
             </div>
             <div className="flex w-1/5 mx-4">
-                <Image className="pb-16 mr-2" src="/property1.svg" alt="Salgstjek ikon" width={32} height={32} />
+                <Image className="mb-16 mr-2 bg-blue-100" src="/location.svg" alt="Salgstjek ikon" width={32} height={32} />
                 <div className="flex flex-col">
                     <h3 className="font-bold text-blue-950">Bestil et salgstjek</h3>
                     <p className="w-fit">Med et Din Mægler Salgstjek 
@@ -22,7 +22,7 @@ export default function InfoCards() {
                 </div>
             </div>
             <div className="flex w-1/5 mx-4">
-                <Image className="pb-16 mr-2" src="/property1.svg" alt="Salgstjek ikon" width={32} height={32} />
+                <Image className="mb-16 mr-2 bg-blue-100" src="/customerBuy.svg" alt="Salgstjek ikon" width={32} height={32} />
                 <div className="flex flex-col">
                     <h3 className="font-bold text-blue-950">Bestil et salgstjek</h3>
                     <p className="w-fit">Med et Din Mægler Salgstjek 
